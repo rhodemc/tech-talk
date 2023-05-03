690b40cace9b34804c1f38128348533c6d233c9d
rhodemc <rhodemc@gmail.com>
2023-05-02 22:29:30 -0700

    site partially works

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,8 +5,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogPostRoutes = require('./blogPostRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
-router.use('/posts', blogPostRoutes);
+router.use('/blogposts', blogPostRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
