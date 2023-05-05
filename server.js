d305109d2fbd6b247c4600ebb76b9289cc8a497d
rhodemc <rhodemc@gmail.com>
2023-05-04 18:05:13 -0700

    updated port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // create the Express.js app and configure the port
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
